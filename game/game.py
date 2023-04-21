70e985b59b54ff9251858c81d4adb3ccf1d4184d
Michal Golichowski <michal.golichowski@bluesoft.com>
2023-04-21 23:23:32 +0200

    Multiple tanks

--- game/game.py
+++ game/game.py
@@ -7,13 +7,19 @@ class Game:
 
         # Create objects
         self.pygame_instance = pygame_instance
-        self.tank = Tank(50, 350)
         self.terrain = Terrain(width, height)
         self.width = width
         self.game_display = pygame_instance.display.set_mode((width, height))
         self.height = height
         self.clock = pygame_instance.time.Clock()
         self.bullets = []
+        self.tanks = [
+            Tank(50, 350),
+            Tank(200, 350),
+            Tank(350, 350),
+            Tank(500, 350),
+        ]
+        self.player_tank = self.tanks[0]  # Reference to the player-controlled tank
 
     def run(self):
         # Game loop
@@ -22,25 +28,26 @@ class Game:
             # Handle events
             keys = self.pygame_instance.key.get_pressed()
             if keys[self.pygame_instance.K_LEFT]:
-                self.tank.move_left()
+                self.player_tank.move_left()
             if keys[self.pygame_instance.K_RIGHT]:
-                self.tank.move_right()
+                self.player_tank.move_right()
             if keys[self.pygame_instance.K_SPACE]:
-                self.tank.jump()
+                self.player_tank.jump()
             for event in self.pygame_instance.event.get():
                 if event.type == self.pygame_instance.QUIT:
                     game_exit = True
                 if event.type == self.pygame_instance.KEYDOWN:
                     if event.key == self.pygame_instance.K_LCTRL:
-                        # Adjust angle based on tank's direction (45 degrees for right, 135 degrees for left)
-                        angle = 45 if self.tank.direction == 1 else 135
-                        bullet = Bullet(self.tank.x + self.tank.width/2, self.tank.y + self.tank.height/2, angle)
+                        angle = 45 if self.player_tank.direction == 1 else 135
+                        bullet = Bullet(self.player_tank.x + self.player_tank.width/2, self.player_tank.y + self.player_tank.height/2, angle)
                         self.bullets.append(bullet)
             # Update screen
             self.game_display.fill((0, 0, 0))
             self.terrain.draw(self.game_display, (0, 255, 0))
-            self.tank.update(self.terrain)
-            self.tank.draw(self.game_display)
+            # Update and draw tanks
+            for tank in self.tanks:
+                tank.update(self.terrain)
+                tank.draw(self.game_display)
             # Update bullets and remove those that hit the ground
             self.bullets = [bullet for bullet in self.bullets if not bullet.update(self.terrain)]
             for bullet in self.bullets:
